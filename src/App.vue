bf9ad25749374e68996a9af14d34c8e8a34bf720
Sercasaurus <nills.van.limbeek@live.nl>
2018-05-31 17:50:03 +0200

    integrated scss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <h1>Test</h1>
+    </div>
 </template>
 
 <script>
@@ -11,13 +10,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+    @import './scss/main.scss';
+
 </style>
