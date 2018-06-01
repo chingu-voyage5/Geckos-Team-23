826a89b04409c886387aa460eb471959c8601878
Sercasaurus <nills.van.limbeek@live.nl>
2018-06-01 22:38:21 +0200

    added vuex & removed HellWorld component

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,13 +2,20 @@
     <div class="index">
         <h1>Main Page</h1>
         <router-link to="/workspaces">Go to workspaces</router-link>
+        <p>{{ helloMsg }}</p>
     </div>
 </template>
 
 <script>
-export default {
+    import { mapState } from 'vuex'
 
-}
+    export default {
+        computed: {
+            ...mapState({
+                helloMsg: 'msg'
+            })
+        }
+    }
 </script>
 
 <style>
