98be1d40b23c2e67b00daf1aaac7a1978a3f7c94
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-06-02 22:37:31 +0400

    basic html and css for workspace dashboard added

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar">
+    <h2>Sidebar</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: red;
+}
+</style>
