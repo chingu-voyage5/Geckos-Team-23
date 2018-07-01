f8c4e44f13fb27dc65e9f6052c97671a49ef5753
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-07-01 16:12:21 +0400

    added original home page routing code

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,6 +6,8 @@
 
 <script>
 import Login from '../components/Login'
+import firebase from 'firebase'
+import { db } from '../main'
 
 export default {
   name: 'Home',
@@ -14,11 +16,58 @@ export default {
 
   },
   data () {
-    return {
+        return {
+          signInEmail: '',
+          signInPassword: '',
+          signUpEmail: '',
+          signUpPassword: '',
+          signedUser: firebase.auth().currentUser,
+					userName
+        }
+    },
+    methods: {
+      goToWorkspaces: function () {
+        this.$router.replace('Dashboard')
+      },
+      signIn: function () {
+        firebase.auth().signInWithEmailAndPassword(this.signInEmail, this.signInPassword).then(
+          // use arrow notation to fix this's context
+          (user) => {
+            this.$router.replace('Dashboard')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        )
+      },
+      signUp: function () {
+        firebase.auth().createUserWithEmailAndPassword(this.signUpEmail, this.signUpPassword).then(
+          // use arrow notation to fix this's context
+          (user) => {
+            // Add user data to DB
+            db.collection("users").add({
+              email: this.signUpEmail,
+							name: this.userName,
+					    image: '',
+							workspaces: []
+            })
+            .then(function (docRef) {
+              console.log('Document written with ID: ', docRef.id)
+            })
+            .catch(function (error) {
+              console.error('Error adding document: ', error)
+            })
 
+            // Redirect to User Workspaces
+            this.$router.replace('Dashboard')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        )
+      }
     }
   }
-}
 </script>
 
 <style>
