8297cc6bca1995ffc07d3c50fd0545536590bfde
Diogo Ribeiro <diogommr@gmail.com>
2018-07-10 18:44:19 +0100

    Set up data loading from DB from dynamic URL.

--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -4,6 +4,10 @@
     <Sidebar />
 
     <div id="workspace-content">
+
+			<p>{{this.$route.params.id}}</p>
+			<p>{{workspace}}</p>
+
       <ul class="workspace__list">
         <Column v-for="column in columns"
             :key="column.id"
@@ -22,12 +26,25 @@
 </template>
 
 <script>
+	import firebase from 'firebase'
+	import { db } from '../main'
   import Sidebar from '../components/Sidebar'
   import Navigation from '../components/Navigation'
   import Column from '../components/Column'
   import { mapState } from 'vuex'
 
   export default {
+		data () {
+			return {
+				userId: firebase.auth().currentUser.uid,
+				workspace: []
+			}
+		},
+		firestore () {
+			return {
+				workspace: db.collection('workspaces').doc(this.$route.params.id)
+			}
+		},
     methods: {
       addColumn () {
         this.$store.commit('addColumn', { title: 'Column', id: this.columns.length + 1 })
