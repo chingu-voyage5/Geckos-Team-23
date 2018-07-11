34dc786317bff47cd3dfcb08c3de63146966042b
Diogo Ribeiro <diogommr@gmail.com>
2018-07-11 13:51:40 +0100

    Fixed minor linting issues.

--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -6,33 +6,33 @@
     <div id="workspace-content">
 
       <ul class="workspace__list">
-		  	<div v-for="(column, idx) in workspace.columns" :key="idx" class="column">
-					<div class="column__header">
-						<input class="column__input" type="text" v-model="column.title" v-on:change="saveWorkspace">
-						<i class="fas fa-ellipsis-v"></i>
+        <div v-for="(column, idx) in workspace.columns" :key="idx" class="column">
+          <div class="column__header">
+            <input class="column__input" type="text" v-model="column.title" v-on:change="saveWorkspace">
+            <i class="fas fa-ellipsis-v"></i>
 
-						<div class="dropdown" v-show="false">
-							<i class="fas fa-caret-up dropdown__arrow"></i>
-							<div class="dropdown__body"></div>
-						</div>
-					</div>
+            <div class="dropdown" v-show="false">
+              <i class="fas fa-caret-up dropdown__arrow"></i>
+              <div class="dropdown__body"></div>
+            </div>
+          </div>
 
-					<div v-for="(item, idx) in column.items" :key="idx" class="item">
-						<div class="item__header" v-bind:style="{ background: workspace.color}">
-							<h3>{{ item.title }}</h3>
-							<i class="fas fa-ellipsis-v"></i>
-						</div>
+          <div v-for="(item, idx) in column.items" :key="idx" class="item">
+            <div class="item__header" v-bind:style="{ background: workspace.color}">
+              <h3>{{ item.title }}</h3>
+              <i class="fas fa-ellipsis-v"></i>
+            </div>
 
-						<div class="item__body">
-							<div class="item__button" v-bind:style="{ background: workspace.color}">
-								<i class="fas fa-plus item__icon"></i>
-							</div>
-						</div>
-					</div>
+            <div class="item__body">
+              <div class="item__button" v-bind:style="{ background: workspace.color}">
+                <i class="fas fa-plus item__icon"></i>
+              </div>
+            </div>
+          </div>
 
-					<div class="column__add-item">
-						<i class="fa fa-plus column__icon"></i>
-					</div>
+          <div class="column__add-item">
+            <i class="fa fa-plus column__icon"></i>
+          </div>
         </div>
       </ul>
 
@@ -54,7 +54,7 @@
     data () {
       return {
         userId: firebase.auth().currentUser.uid,
-				newColTitle: 'New Column ',
+        newColTitle: 'New Column ',
         workspace: []
       }
     },
@@ -64,28 +64,28 @@
       }
     },
     methods: {
-			saveWorkspace () {
-				db.collection('workspaces').doc(this.$route.params.id).set(this.workspace)
-			},
-      addColumn () {
-				// retrieve DB columns
-				let columns = this.workspace.columns;
-
-				// create new column
-				const data = {
-					title: this.newColTitle + (this.workspace.columns.length + 1),
-					items: []
-				}
-
-				// Add column to workspace
-				this.workspace.columns.push(data)
-
-				// Save workspace snapshot to DB
-				this.saveWorkspace()
+      saveWorkspace () {
+        db.collection('workspaces').doc(this.$route.params.id).set(this.workspace)
       },
-			addItem () {
+      addColumn () {
+        // retrieve DB columns
+        let columns = this.workspace.columns
 
-			}
+        // create new column
+        const data = {
+          title: this.newColTitle + (this.workspace.columns.length + 1),
+          items: []
+        }
+
+        // Add column to workspace
+        this.workspace.columns.push(data)
+
+        // Save workspace snapshot to DB
+        this.saveWorkspace()
+      },
+      addItem () {
+
+      }
     },
     components: {
       Sidebar
