0f3de554b1388f497c20a72a97599eb23462852d
Diogo Ribeiro <diogommr@gmail.com>
2018-07-11 20:22:55 +0100

    Fixed linting issues.

--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -15,8 +15,8 @@
             <div class="dropdown">
               <i class="fas fa-caret-up dropdown__arrow"></i>
               <div class="dropdown__body">
-								<button v-on:click="deleteColumn($event)"><i class="fas fa-trash"></i> Delete</button>
-							</div>
+                <button v-on:click="deleteColumn($event)"><i class="fas fa-trash"></i> Delete</button>
+              </div>
             </div>
           </div>
 
@@ -80,37 +80,37 @@
         // Save workspace snapshot to DB
         this.saveWorkspace()
       },
-			toggleDropDown (event) {
-				const dropDownMenu = event.target.nextElementSibling
-				const allMenus = document.querySelectorAll('.dropdown')
-				const clickedMenu = Array.prototype.indexOf.call(allMenus, dropDownMenu)
+      toggleDropDown (event) {
+        const dropDownMenu = event.target.nextElementSibling
+        const allMenus = document.querySelectorAll('.dropdown')
+        const clickedMenu = Array.prototype.indexOf.call(allMenus, dropDownMenu)
 
-				// close all open menus except clicked one
-				for (let i = 0; i < allMenus.length; i++) {
-					if (allMenus[i] !== allMenus[clickedMenu]) {
-						allMenus[i].classList.remove('visible')
-					}
-				}
+        // close all open menus except clicked one
+        for (let i = 0; i < allMenus.length; i++) {
+          if (allMenus[i] !== allMenus[clickedMenu]) {
+            allMenus[i].classList.remove('visible')
+          }
+        }
 
-				// open clicked menu
-				dropDownMenu.classList.toggle('visible')
-			},
-			deleteColumn (event) {
-				const column = event.target.parentNode.parentNode.parentNode.parentNode
-				const allMenus = document.querySelectorAll('.dropdown')
+        // open clicked menu
+        dropDownMenu.classList.toggle('visible')
+      },
+      deleteColumn (event) {
+        const column = event.target.parentNode.parentNode.parentNode.parentNode
+        const allMenus = document.querySelectorAll('.dropdown')
 
-				const workspaceColumns = document.getElementsByClassName('workspace__list')[0]
-				const columnIndex = Array.prototype.indexOf.call(workspaceColumns.children, column)
+        const workspaceColumns = document.getElementsByClassName('workspace__list')[0]
+        const columnIndex = Array.prototype.indexOf.call(workspaceColumns.children, column)
         this.workspace.columns.splice(columnIndex, 1)
 
-				// close all open menus
-				for (let i = 0; i < allMenus.length; i++) {
-					allMenus[i].classList.remove('visible')
-				}
+        // close all open menus
+        for (let i = 0; i < allMenus.length; i++) {
+          allMenus[i].classList.remove('visible')
+        }
 
-				// Save workspace snapshot to DB
+        // Save workspace snapshot to DB
         this.saveWorkspace()
-			},
+      },
       addItem (event) {
         const column = event.target.parentNode
         const columnItems = column.getElementsByClassName('column-items')[0]
