eb26e2e4b0837b93d374f78dacc2970142cde522
Diogo Ribeiro <diogommr@gmail.com>
2018-07-14 12:02:33 +0100

    Finished Dashboard page refactoring.

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -4,9 +4,9 @@
     <Sidebar />
 
     <div id="dashboard-content">
-      <!-- <Navigation /> -->
       <DashboardView />
     </div>
+
   </div>
 </template>
 
@@ -16,12 +16,6 @@ import DashboardView from '../components/DashboardView'
 
 export default {
   name: 'Dashboard',
-  data () {
-    return {
-    }
-  },
-  methods: {
-  },
   components: {
     Sidebar,
     DashboardView
@@ -30,15 +24,18 @@ export default {
 </script>
 
 <style lang="scss">
-.dashboard-container {
-  display: flex;
-  height: 100vh;
-}
-#dashboard-content {
-  width: 100%;
-  padding: 20px 40px;
-  display: flex;
-  justify-content: center;
-	overflow-y: auto;
-}
+
+	.dashboard-container {
+	  display: flex;
+	  height: 100vh;
+	}
+
+	#dashboard-content {
+	  width: 100%;
+	  padding: 20px 40px;
+	  display: flex;
+	  justify-content: center;
+		overflow-y: auto;
+	}
+
 </style>
