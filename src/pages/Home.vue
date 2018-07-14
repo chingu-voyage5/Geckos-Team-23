22807912931f81e4d1ff56cf340073d2853fe94a
Diogo Ribeiro <diogommr@gmail.com>
2018-07-14 14:08:50 +0100

    Home page refactoring.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,66 +5,14 @@
 </template>
 
 <script>
-import Login from '../components/Login'
-import Signup from '../components/Signup'
 import firebase from 'firebase'
-import { db } from '../main'
+import db from '../components/firebaseInit'
+import Login from '../components/Login'
 
 export default {
   name: 'Home',
   components: {
-    'Login': Login,
-    'Signup': Signup
-  },
-  data () {
-    return {
-      signInEmail: '',
-      signInPassword: '',
-      signUpEmail: '',
-      signUpPassword: '',
-      signedUser: firebase.auth().currentUser,
-      userName: ''
-    }
-  },
-  methods: {
-    goToWorkspaces: function () {
-      this.$router.replace('Dashboard')
-    },
-    signIn: function () {
-      firebase.auth().signInWithEmailAndPassword(this.signInEmail, this.signInPassword).then(
-        // use arrow notation to fix this's context
-        (user) => {
-          this.$router.replace('Dashboard')
-        },
-        (err) => {
-          alert('Oops. ' + err.message)
-        }
-      )
-    },
-    signUp: function () {
-      firebase.auth().createUserWithEmailAndPassword(this.signUpEmail, this.signUpPassword).then(
-        // use arrow notation to fix this's context
-        (user) => {
-          // Add user data to DB
-          db.collection('users').add({
-            email: this.signUpEmail,
-            userName: this.userName
-          })
-          .then(function (docRef) {
-            console.log('Document written with ID: ', docRef.id)
-          })
-          .catch(function (error) {
-            console.error('Error adding document: ', error)
-          })
-
-          // Redirect to User Workspaces
-          this.$router.replace('Dashboard')
-        },
-        (err) => {
-          alert('Oops. ' + err.message)
-        }
-      )
-    }
+    'Login': Login
   }
 }
 </script>
