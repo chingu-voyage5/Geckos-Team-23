7e2ed1a2b21709d1dee6b0f55c6c68e515087791
Corné <cornebotma@hotmail.com>
2018-07-30 17:49:18 +0200

    fixed conflicts

--- src/main.js
+++ src/main.js
@@ -3,24 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
-import firebase from 'firebase'
-=======
 import store from './store'
 import VModal from 'vue-js-modal'
 import firebase from 'firebase'
 
 Vue.use(VModal)
->>>>>>> add-item
 
 Vue.config.productionTip = false
 let app
 
-<<<<<<< HEAD
-firebase.initializeApp(config)
-=======
 // Initialize the app only when we are sure Firebase Auth object is ready to use.
->>>>>>> add-item
 firebase.auth().onAuthStateChanged(function (user) {
   if (!app) {
     /* eslint-disable no-new */
